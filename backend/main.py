e0fe74de3ed7199a123c6aa029f5a95d7e18f84d
Muhammad Farooq <muhammad.farooq@ocloudsolutions.net>
2025-07-24 23:16:01 +0500

    Setup CORS

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ security = HTTPBearer()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://yfinance-stocks.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
